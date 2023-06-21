d45e949076915b6a4d8f806f6b0e9ea2f86a614c
tusharsurani <137286868+tusharsurani@users.noreply.github.com>
2023-06-21 16:16:02 +0530

    Add files via upload

---
+++ SECOND.C
@@ -0,0 +1,36 @@
+  #include<stdio.h>
+  #include<conio.h>
+ void main()
+  {
+  clrscr();
+  printf("tushar surani\n");
+  printf("my age are 18\n");
+  printf("i studyded at l.d college of ahmedabad\n");
+  printf(" - - - - - - - \n");
+  printf("|             |\n");
+  printf("|SURANI       |\n");
+  printf("|  TUSHAR     |\n");
+  printf("|    KANTIBHAI|\n");
+  printf("|             |\n");
+  printf(" - - - - - - - \n");
+  printf(" - - - -\n");
+  printf("|\t|\n");
+  printf("T\t|\n");
+  printf("U\t|\n");
+  printf("S\t|\n");
+  printf("H\t|\n");
+  printf("A\t|\n");
+  printf("R\t|\n");
+  printf("|\t|\n");
+  printf(" - - - -\n");
+  printf("*\n");
+  printf("*                * *\n");
+  printf("*               *   *   *\n");
+  printf("*    * *       *     * *\n");
+  printf("*   *   *     *\n");
+  printf("*  *     *   *\n");
+  printf("* *       * *\n");
+  printf("*\n");
+  getch();
+
+  }
